0257caa20dcbf8518a9c84ddfb47e4e5276220cc
ymerenich1998 <ymerenich1998@gmail.com>
2018-10-09 23:13:47 +0300

    add design in first page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,27 @@
 //= require turbolinks
 //= require_tree .
 
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 1) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
