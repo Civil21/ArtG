5cc167548201b5adb16be1c7b332d43ce58c044f
Civil21 <diusolexander@gmail.com>
2018-10-12 14:34:14 +0300

    create filte from all_sum_con to categories_arts

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -2,14 +2,35 @@ class ArtsController < ApplicationController
 	before_action :aut_artist!, except:[:show,:index]
 	before_action :get_art,except:[:index,:new,:create]
 	before_action :get_artist, except:[:index]
+  before_action :get_groups, only:[:new,:edit]
 
 	def index
+    #@categories=Category.all
+
     if(Artist.find_by(name: params[:artist_id])|| Artist.find_by(id: params[:artist_id]))
       @artist = Artist.find_by(name: params[:artist_id])
       @artist ||=Artist.find(params[:artist_id])
       @arts=Art.where(artist_id: @artist.id)
     else
-      @arts=Art.all
+
+      #Фільтр за категоріями  при наявності категорій для фільтрування вибирає тільки їх спільні випадки 
+      #По можливості зробити фільтр , щоб у випаду відсутності варіантів по вибраним категоріям знаходив ймовірні комбінаціїї які підпадають під задані параметри пошуку. ТОбто не загальна сума а сума відносна , випадки без однієїї або без декількох
+      if(!@categories)
+        @arts=Art.all
+      else
+        @arts = Art.ids
+        @categories.each{|category|
+          @arts = @arts & category.arts.ids
+        }
+        if(!@arts)
+          @arts = []
+        else
+          @arts.map{|art|
+            pp art=art.to_s
+          }
+          @arts = Art.find(@arts)
+        end
+      end
     end
   end
 
@@ -19,14 +40,10 @@ class ArtsController < ApplicationController
 
 	def new
     @navbar_off = true	
-    @groups = CategoryGroup.all
-    @categories = []
-    @groups.each{|group|
-      @categories[group.id] = Category.where(category_group_id: group.id)
-      }
   end
 
   def create
+    #pp params[:categories_ids]
   	params[:art][:artist_id]=@current_artist.id
     @categories = Category.find(params[:categories_ids])
   	@art=Art.create(art_params)
@@ -72,5 +89,13 @@ class ArtsController < ApplicationController
 
   def art_params
   	params.require(:art).permit(:name,:description,:artist_id,:price,:picture)
+  end
+
+  def get_groups
+    @groups = CategoryGroup.all
+    @categories = []
+    @groups.each{|group|
+      @categories[group.id] = Category.where(category_group_id: group.id)
+      }
   end
 end
