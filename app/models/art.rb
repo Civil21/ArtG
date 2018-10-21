c11379df7719aaf35f97224cbb2f8536f6d208b7
Civil21 <diusolexander@gmail.com>
2018-10-21 16:59:15 +0300

    deploy task

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,6 +2,6 @@ class Art < ApplicationRecord
 	belongs_to :artist
 	has_and_belongs_to_many :categories
 
-	has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :picture, styles: { medium: "250x250>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
